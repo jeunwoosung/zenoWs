3347913d52b318c570d77b6dcc35da718625028a
zenoWS <zeno9302@naver.com>
2024-07-25 04:09:44 +0900

    3. 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력합니다.     - 사칙연산 기호에 맞는 연산자를 사용하여 연산을 진행합니다.     - 입력받은 연산 기호를 구분하기 위해 제어문을 사용합니다. (e.g.if, switch)     - 연산 오류가 발생할 경우 해당 오류에 대한 내용을 정제하여 출력합니다.       ex) 나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.

--- App.java
+++ App.java
@@ -18,8 +18,30 @@ public class App {
         System.out.println("사칙연산 기호를 입력하세요: ");
         char operator = sc.next().charAt(0);
 
-        System.out.println("operator = " + operator);
+        int result = 0;
 
+        switch (operator) {
+            case '+' :
+                result = num1 + num2;
+            break;
+
+            case '-' :
+                result = num1 - num2;
+                break;
+
+            case '*' :
+                result = num1 * num2;
+                break;
+
+            case '/' :
+                if (num2 == 0){
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다");
+                } else {
+                    result = num1 / num2;
+                }
+                break;
+        }
+        System.out.println("결과 : " + result);
     }
 }
 
