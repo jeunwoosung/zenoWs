2ff8d7e019b9784253261b6c97d7b427fe46702c
zenoWS <zeno9302@naver.com>
2024-07-31 05:11:25 +0900

    2-1  양의 정수 2개(0 포함)와 연산 기호를 매개변수로 받아 사칙연산(+,-,*,/) 기능을 수행한 후 결과 값을 반환하는 메서드와 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성합니다.  - 나눗셈에서 분모에 0이 들어오거나 연산자 기호가 잘 못 들어온 경우 적합한 Exception 클래스를 생성하여 throw 합니다. (매개변수로 해당 오류 내용을 전달합니다.)

---
+++ Calculator.java
@@ -0,0 +1,65 @@
+package calculator;
+
+import java.util.LinkedList;
+
+
+public class Calculator {
+   private LinkedList<Double> linkedlist = new LinkedList<>(); //연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성
+
+
+    public double calculate (double num1, double num2, char operator) { // App의 Main메소드에서 num1,num2,operator를 사용자 입력값을 받은뒤 연산에 적용하기위한 매개변수.
+
+        double result = 0; //결과값은 0부터 시작해라.
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+
+            case '-':
+                result = num1 - num2;
+                break;
+
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                if (num2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력 될 수 없습니다.");
+                } else {
+                    result = num1 / num2;
+
+                }
+                break;
+
+            default:
+                System.out.println("알수없는 연산입니다.");
+
+
+
+
+        }
+
+
+
+
+        linkedlist.add(result); // linkedlist에 결과값을 추가한다.
+        return result; //리턴값은 결과값이 되게 만듬
+
+
+
+
+    }
+
+
+    public LinkedList<Double> getLinkedlist() { // LinkedList<double> 타입의 getLinkedlist라는 메서드를 생성하고
+        return new LinkedList<>(linkedlist); // 새로운 LinkedList타입의 linkedlist를 생성하고 복사해서 내보낸다.
+    }
+
+    public void setLinkedlist(LinkedList<Double> linkedlist) {//LinkedList<Double>타입의 linkedlist의 매개변수를 정해서
+        this.linkedlist = linkedlist; // private접근제어자 필드에 Stter메서드를 통해 값을 저장하거나 수정할수 있다.
+    }
+
+
+    }
